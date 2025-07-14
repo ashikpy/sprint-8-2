ac1a5715ad468a0e92ce34aaf476c709e36440ba
Ashik D <ashikofficial455@gmail.com>
2025-07-14 13:18:28 +0530

    finisehd

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,14 @@ function App() {
     <div>
       <Navbar />
       <div className="container max-w-3xl flex flex-col gap-10 my-20 mx-auto">
-        <h1 className="text-3xl font-bold">Expense Tracker</h1>
+        <div className="mb-10">
+          <h1 className="text-5xl text-center font-extrabold ">
+            Expense Tracker
+          </h1>
+          <p className="text-lg opacity-50 font-semibold  mt-1 text-center">
+            Track your expenses easily
+          </p>
+        </div>
         <div className="flex gap-2 justify-between items-stretch">
           <Hero />
           <div className="flex-1 h-auto">
